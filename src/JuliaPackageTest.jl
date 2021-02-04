5599b2f00a7bf1f4e2ff71da03b1754c9e8dca25
jeff788 <jeff788@gmail.com>
2021-02-04 19:02:42 +0000

    added a test and a dependency

--- src/JuliaPackageTest.jl
+++ src/JuliaPackageTest.jl
@@ -1,5 +1,9 @@
 module JuliaPackageTest
 
+using Distributions
+
 # Write your package code here.
 
+export sum_values
+include("simple_function.jl")
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,7 @@ using Test
 
 @testset "JuliaPackageTest.jl" begin
     # Write your tests here.
+    x = 2
+    y = 3
+    @test JuliaPackageTest.sum_values(x,y) == 5
 end
